91e5e813fcca3cc81127f484aaf67d977d053400
Lahiru Sadaruwan <codedevoption@gmail.com>
2022-12-31 18:08:21 +0530

    Pages Implemented Successed

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -13,12 +13,12 @@ import {
 } from "@syncfusion/ej2-react-grids";
 
 import { Header } from "../components/";
-import { customersData, customersGrid, employeesData, employeesGrid } from "../assets/data/dummy";
+import { customersData, customersGrid} from "../assets/data/dummy";
 
 const Customers = () => {
   return (
     <div className="m-2 mt-16 md:mt-0 md:m-5 md:p-5 2xl::m-10 p-2 2xl:p-10">
-      <Header category="Page" title="Employees" />
+      <Header category="Page" title="Customers" />
       <GridComponent width='auto' toolbar={['Delete']} dataSource={customersData} editSettings={{allowDeleting:true,allowEditing:true}} allowPaging allowSorting>
         <ColumnsDirective>
           {customersGrid.map((item, index) => (
