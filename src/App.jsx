f1d508630e7744c3f22a1232159d77cc31641e78
Lahiru Sadaruwan <codedevoption@gmail.com>
2022-12-31 15:30:37 +0530

    implemented successed

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
   const [{activeMenu}, dispatch] = useStateValue();
   return (
     <div>
-      <div className="flex relative dark:bg-main-dark-bg">
+      <div className="flex relative  dark:bg-main-dark-bg">
         {/* Setting Btn Bottom Right Fixed */}
         <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
           <TooltipComponent content="Settings" position="Top">
